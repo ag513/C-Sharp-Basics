c4fae51469dc2f0cd46564775fa22835be3a9640
Abhinav Gunishetty <abhinav.gunishetty@steinias.com>
2021-08-30 15:18:27 +0100

    Abstract classes

---
+++ AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a = new Class1();
+            // MyAbstractClass b = new MyAbstractClass();
+            a.PrintMessage();
+            a.PrintMessageAbstract();
+            Console.Read();
+        }
+    }
+
+    abstract class MyAbstractClass
+    {
+        private string message = "Hello c#";
+        public void PrintMessage() {
+            Console.WriteLine(message);
+        }
+        public abstract void PrintMessageAbstract();
+    }
+   
+    class Class1 : MyAbstractClass
+    {
+        public override void PrintMessageAbstract()
+        {
+          Console.WriteLine("c# is fun");
+        }
+    }
+}
