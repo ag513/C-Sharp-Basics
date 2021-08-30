33c104c40890ad4a9ad8891729cc2aa3337f4967
Abhinav Gunishetty <abhinav.gunishetty@steinias.com>
2021-08-30 15:00:06 +0100

    Polymoprhism

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -1,3 +1,43 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NormalMember member1 = new NormalMember("Special rate", "James", 1, 2010);
+            //VIPMember member2 = new VIPMember("Abhi", 2, 2122);
+            //member1.CalculateAnnualFee();
+            //member2.CalculateAnnualFee();
+
+            //Console.WriteLine(member1.ToString());
+            //Console.WriteLine(member2.ToString());
+
+            // POLYMORPHISM
+            Member[] clubMembers = new Member[5];
+            clubMembers[0] = new NormalMember("Special Rate", "James", 1,
+            2010);
+            clubMembers[1] = new NormalMember("Normal Rate", "Andy", 2,
+            2011);
+            clubMembers[2] = new NormalMember("Normal Rate", "Bill", 3,
+            2011);
+            clubMembers[3] = new VIPMember("Carol", 4, 2012);
+            clubMembers[4] = new VIPMember("Evelyn", 5, 2012);
+
+            foreach(Member m in clubMembers)
+            {
+                m.CalculateAnnualFee();
+                Console.WriteLine(m.ToString());
+            }
+
+            if (clubMembers[0].GetType() == typeof(VIPMember))
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+        }
+    }
+
     class Member
     {
         protected int annualFee;
